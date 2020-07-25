e2c3073273511f6d1a2ac93bfab84279bbd49d67
rickyriosp <riki.rios91@gmail.com>
2020-07-24 23:39:37 -0400

    initial commit

---
+++ src/components/CardList.js
@@ -0,0 +1,21 @@
+import Card from "./Card.js";
+import React from "react";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((user, i) => {
+        return (
+          <Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
---
+++ src/components/Scroll.js
@@ -0,0 +1,19 @@
+import "./Scroll.css";
+
+import React from "react";
+
+const Scroll = (props) => {
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        border: "3px solid black",
+        height: "800px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
